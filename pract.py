c44d4a4259992032f5afd2dd2983211d9e719e9b
Mang-Khongsai <mangneokhsai93@gmail.com>
2024-12-24 13:32:42 +0530

    opencv

--- pract.py
+++ pract.py
@@ -1,5 +1,19 @@
+import numpy as np 
 
 height = 200
 width = 290
 
 print(height//2)
+
+arr = np.zeros((2,3,3))
+for i in range(arr.shape[0]):
+    for j in range(arr.shape[1]):
+        for k in range(arr.shape[2]):
+            arr[i,j,k] = int(np.random.randint(0,256))
+            
+print(arr[0,0])
+print(arr[0][0])
+print(arr[0,0,2])
+print(arr)
+print(arr[0,1,2])
+print(arr[0][1][2])
--- t5_color_color_detect.py
+++ t5_color_color_detect.py
@@ -4,13 +4,20 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    width = int(cap.get(3))
-    height = int(cap.get(4))
+    width = int(cap.get(3)) # measure the width of the frame
+    height = int(cap.get(4)) # measures the height of the frame
     
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imshow('frame', hsv)
+    lower_blue  = np.array([110, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+    
+    cv2.imshow('frame', result)
+    cv2.imshow('mash', mask)
     if cv2.waitKey(1) == ord('q'):
         break 
-    
+        
 cap.release()
 cv2.destroyAllWindows()
